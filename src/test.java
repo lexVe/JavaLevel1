c07c1801b5db2137e5787730e8c5bd81249aef49
lexVe <cnah4@rambler.ru>
2020-09-02 14:55:18 +0300

    1-5
    6,7 tasks left

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class test {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][10];
+        for(int i=0; i< arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
