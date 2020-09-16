9b81f57c5060cbcbf1e2210d96179030ce01c65a
lexVe <cnah4@rambler.ru>
2020-09-16 21:30:18 +0300

    Lessong6
    Done

---
+++ src/lesson6/animals/Dog.java
@@ -0,0 +1,8 @@
+package lesson6.animals;
+
+public class Dog extends animal {
+
+    Dog(String name, int runLimit, int swimLimit, int jumpLimit){
+        super(name, runLimit, swimLimit, jumpLimit);
+    }
+}
